db774d00a9f3ceb3dfccfdec751368b6c952d185
James Ruan <ruanbeihong@gmail.com>
2014-02-05 22:09:46 +0800

    add login.html javascript/login.js

---
+++ javascripts/login.js
@@ -0,0 +1,11 @@
+function buildErrorMessage(e) {
+    return e.line !== undefined && e.column !== undefined
+		? "Line " + e.line + ", column " + e.column + ": " + e.message
+    	: e.message;
+}
+
+$(document).ready(function () {
+  var parsed_ast = pegjs.parse($("#content").text());
+  $("#content").jsml(parsed_ast);
+  prettyPrint();
+});
